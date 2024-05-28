f402e71ce501dae14c19d52b585f33118224a37b
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-05-28 12:00:44 +0200

    image -> OK

--- info.php
+++ info.php
@@ -11,7 +11,7 @@ catch (Exception $e)
     die('Erreur : ' . $e->getMessage());
 }
 
-$sqlQuery = 'SELECT DISTINCT ingredient_name, recipe_ingredient.quantity, ingredient.unity, recipe.preparation_time, recipe.instructions, recipe.image
+$sqlQuery = 'SELECT DISTINCT recipe_name, ingredient_name, recipe_ingredient.quantity, ingredient.unity, recipe.preparation_time, recipe.instructions, recipe.image
 FROM ingredient
 INNER JOIN recipe_ingredient
 ON ingredient.id_ingredient = recipe_ingredient.id_ingredient
@@ -27,6 +27,7 @@ $recipes = $recipeStatement->fetchAll();
 
 echo "<table>
         <tr>
+            <th> Nom de la recette </th>
             <th>Ingredient</th>
             <th>Quantité</th>
             <th>Temps de préparation</th>
@@ -36,12 +37,15 @@ echo "<table>
 
         foreach ($recipes as $recipe) {
             echo "<tr>
+            <td>".$recipe['recipe_name']."</td>
           <td>".$recipe['ingredient_name']."</td>
-          <td>".$recipe['quantity'].$recipe['unity']."</td>
-          <td>".$recipe['preparation_time']."</td>
-          <td>".$recipe['instructions']."</td>
-          <td>".$recipe['image']."</td>";
+          <td>".$recipe['quantity'].$recipe['unity']."</td>";
         }
+
+    echo 
+        "<td>".$recipe['preparation_time']."</td>
+          <td>".$recipe['instructions']."</td>
+          <img src='{$recipe['image']}' alt='Image de la recette'/>";
 
         echo "</table>"
 
