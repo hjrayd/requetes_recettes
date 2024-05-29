af4c2fdd31fe18c894ca4a1fdc305b75b15776de
Hajar Ayaddouz <hajarayaddouz29@gmail.com>
2024-05-29 09:15:39 +0200

    Ingredient stock data base -> OK

--- traitement.php
+++ traitement.php
@@ -1,5 +1,14 @@
 <?php 
     session_start();
+    try
+{
+    $mysqlClient = new PDO('mysql:host=localhost;dbname=recettes_hajar;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+
     if(isset($_GET['action'])) {
         switch($_GET['action']) {
 
@@ -8,6 +17,7 @@
                     $nom = filter_input(INPUT_POST, "nom", FILTER_SANITIZE_FULL_SPECIAL_CHARS); 
                     $unity = filter_input(INPUT_POST, "unity", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
                     $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+
                    
     
                         if($nom && $unity && $price) {
@@ -17,14 +27,15 @@
                             "unity"=> $unity,
                             "price" => $price,
                             ];
+
                    
-                            $sql = 'INSERT INTO ingredient (ingredient_name, unity, price)
-                            VALUES (:nom, :unity, :price)';
-                            $recetteStatement = $donn-> prepare($sql);
-                            $recetteStatement->execute([$ingredient]);
+                    
+                            $sql = "INSERT INTO ingredient (ingredient_name, unity, price)
+                            VALUES (:nom, :unity, :price)";
+                             $recetteStatement = $mysqlClient->prepare($sql);
+                             $recetteStatement->execute($ingredient);         
                         }
-                    }
-                
+                }
                     header("Location:recettes.php");
                     break;
                 
